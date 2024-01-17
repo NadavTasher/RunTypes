a1343d1556a476a49077cc5908016afacf007b1c
Nadav Tasher <tashernadav@gmail.com>
2024-01-17 23:08:27 +0200

    FIxed namedtuple bug on Python 2

--- src/runtypes/typedtuple.py
+++ src/runtypes/typedtuple.py
@@ -4,28 +4,26 @@ import collections
 
 def TypedTuple(name, fields):
     # Create namedtuple classtype
-    namedtuple = collections.namedtuple(name, [key for key, _ in fields])
+    original_class = collections.namedtuple(name, [key for key, _ in fields])
 
-    # Fetch the original __new__ function
-    original_constructor = namedtuple.__new__
+    # Create the subclass from the original class
+    class modified_class(original_class):
 
-    # Create __new__ function
-    @functools.wraps(original_constructor)
-    def __new__(cls, *args, **kwargs):
-        # Initialize namedtuple with values
-        self = original_constructor(cls, *args, **kwargs)
+        def __new__(cls, *args, **kwargs):
+            # Initialize namedtuple with values
+            self = original_class.__new__(cls, *args, **kwargs)
 
-        # Type-check and replace
-        self = self._replace(**{key: value_type(getattr(self, key)) for key, value_type in fields})
+            # Type-check and replace
+            self = self._replace(**{key: value_type(getattr(self, key)) for key, value_type in fields})
 
-        # Return the new tuple
-        return self
+            # Return the new tuple
+            return self
 
-    # Replace the __new__ function
-    namedtuple.__new__ = __new__
+    # Replace the name with the original name
+    modified_class.__name__ = name
 
-    # Return the namedtuple class
-    return namedtuple
+    # Return the modified class
+    return modified_class
 
 
 # Create lower-case name for ease-of-use

4cc855192a9b7d86d83a6c593c6b8e5a89b6f164
Nadav Tasher <tashernadav@gmail.com>
2024-10-13 20:32:53 +0300

    Explicit exports

--- src/runtypes/__init__.py
+++ src/runtypes/__init__.py
@@ -1,6 +1,6 @@
 # Import runtypes
-from runtypes.types.basic import *
-from runtypes.types.advanced import *
+from runtypes.types.basic import Any, Union, Literal, Optional, Text, AnyStr, ByteString, Float, Integer, Boolean, List, Dict, Tuple
+from runtypes.types.advanced import Schema, Charset, Path, PathName, Email, Domain, Pattern, ID, Binary, Decimal, Hexadecimal
 
 # Import type hint utilities
 from runtypes.hints import cast_type_hints, check_type_hints, typecast, typecheck
@@ -10,3 +10,6 @@ from runtypes.tuples import TypedTuple, typedtuple
 
 # Import other utilities
 from runtypes.runtype import RunType, ArgumentError, typechecker
+
+# Add explicit exports
+__all__ = ["Any", "Union", "Literal", "Optional", "Text", "AnyStr", "ByteString", "Float", "Integer", "Boolean", "List", "Dict", "Tuple", "Schema", "Charset", "Path", "PathName", "Email", "Domain", "Pattern", "ID", "Binary", "Decimal", "Hexadecimal", "cast_type_hints", "check_type_hints", "typecast", "typecheck", "TypedTuple", "typedtuple", "RunType", "ArgumentError", "typechecker"]
